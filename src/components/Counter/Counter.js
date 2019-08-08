baa499d6a9dcff17d6d76342fca3dd9634fdac79
Realetive <realetive@yandex.ru>
2019-08-08 11:58:57 +0300

    Fix counter's number

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -11,7 +11,7 @@ const Counter = ({ _key, tickets, setTickets, defaultValue }) => {
   return (
     <>
       <button type='button' className='counterBtn' disabled={ count <= 0 } onClick={ () => _setValue( count - 1 ) }>–</button>
-      <input type='number' className='counterInput' min={ 0 } value={ count } onChange={ e => _setValue(e.target.value) } />
+      <input type='number' className='counterInput' min={ 0 } value={ count } onChange={ e => _setValue( parseInt( e.target.value ) ) } />
       <button type='button' className='counterBtn' onClick={ () => _setValue( count + 1 ) }>+</button>
     </>
   );
