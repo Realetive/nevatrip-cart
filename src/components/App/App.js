b6e0a3bb1ae990e349ebc132680f3ddc4673fa66
Realetive <realetive@yandex.ru>
2019-08-08 11:59:25 +0300

    Remove Catcher

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import StoreContext from 'storeon/react/context';
 
-//import Catcher from "../Catcher/Catcher";
 import { Cart } from "../Cart/Cart";
 import { store } from "../../state";
 
@@ -9,10 +8,8 @@ import './App.css';
 
 export default function App({session}) {
   return (
-    // <Catcher>
-      <StoreContext.Provider value={ store }>
-        <Cart session={ session } />
-      </StoreContext.Provider>
-    // </Catcher>
+    <StoreContext.Provider value={ store }>
+      <Cart session={ session } />
+    </StoreContext.Provider>
   );
 }
