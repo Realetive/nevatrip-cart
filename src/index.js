9b7850161a0beb6d4c368f59968006290bdfcddc
Realetive <realetive@yandex.ru>
2020-04-23 22:42:13 +0300

    Fix current language

--- src/index.js
+++ src/index.js
@@ -10,29 +10,29 @@ const session = new URL( window.location.href ).searchParams.get( 'session' )
                 || root.dataset.session
                 || 'test-test-test';
 
-const html = document.querySelector('html');
-const [ currentLang, isRightTranslate ] = i18n(lang);
+const html = document.documentElement;
+const [ currentLang, isRightTranslate ] = i18n( lang );
 
-const observer = new MutationObserver(function(mutationsList) {
-    for (const mutation of mutationsList) {
-        const attr = mutation.attributeName;
-        const lang = mutation.target.getAttribute(attr);
-        const [ currentLang, isRightTranslate ] = i18n(lang);
-
-        render(
-            <App session={session} lang={currentLang} isRightTranslate={isRightTranslate} />,
-            root
-        );
-    }
+const observer = new MutationObserver( function(mutationsList) {
+  for (const mutation of mutationsList) {
+    const attr = mutation.attributeName;
+    const lang = mutation.target.getAttribute(attr);
+    const [ currentLang, isRightTranslate ] = i18n(lang);
+    
+    render(
+      <App session={ session } lang={ currentLang } isRightTranslate={ isRightTranslate } />,
+      root
+    );
+  }
 });
-
+  
 observer.observe(html, { attributeFilter: ['lang'] });
-
+  
 render(
-  <App session={ session } lang={ lang } />,
+  <App session={ session } lang={ currentLang } isRightTranslate={ isRightTranslate } />,
   root
 );
-
+    
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
