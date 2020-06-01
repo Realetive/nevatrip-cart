3830bd3c1d7cb3720bc75fe70eda28eff4081b3e
olbol <olbolot@gmail.com>
2020-06-01 15:03:33 +0300

    Remove folder with states

--- src/state/event.js
+++
@@ -1,7 +0,0 @@
-export default (store) => {
-  store.on('@init', () => ({ event: {} }));
-
-  store.on('event/add', ({ event }, newEvent) => {
-    return { event: { ...event, ...newEvent } };
-  });
-};
--- src/state/session.js
+++
@@ -1,5 +0,0 @@
-export default (store) => {
-  store.on('@init', () => ({ session: null }));
-
-  store.on('session/id', (state, session) => ({ session }));
-};
