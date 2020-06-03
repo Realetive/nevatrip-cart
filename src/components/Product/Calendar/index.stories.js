7b0fcc66176cc5c8bd6c2fc20b21b4881ecc2405
olbol <olbolot@gmail.com>
2020-06-03 12:46:07 +0300

    Fix calendar story

--- src/components/Product/Calendar/index.stories.js
+++ src/components/Product/Calendar/index.stories.js
@@ -6,41 +6,37 @@ import './Calendar.css';
 import '../../App/App.css';
 import CalendarReadme from './README.md';
 
-function generateDates( length, timeStr = 'future', isSort = true ) {
+const generateDates = ( length, timeStr = 'future', isSort = true ) => {
   const time = ( timeStr === 'future' && 1 ) || ( timeStr === 'past' && -1 );
   const datesArray = [];
   const start = new Date();
-  const end = new Date(new Date().setFullYear(new Date().getFullYear() + time));
+  const end = new Date( new Date().setFullYear( new Date().getFullYear() + time ) );
 
-  const randomDate = (start, end) => {
-    const date = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
-    date.setHours('0');
-    date.setMinutes('0');
-    date.setSeconds('0');
+  const randomDate = ( start, end ) => {
+    const date = new Date( start.getTime() + Math.random() * ( end.getTime() - start.getTime() ) );
+    date.setHours( '0' );
+    date.setMinutes( '0' );
+    date.setSeconds( '0' );
 
     return date.toString();
   }
 
-  for (let i = 0; i < length; i++) {
-    const currentDate = randomDate(start, end);
+  for ( let i = 0; i < length; i++ ) {
+    const currentDate = randomDate( start, end );
 
-    if (!datesArray.includes(currentDate)) {
-      datesArray.push(currentDate);
+    if ( !datesArray.includes( currentDate ) ) {
+      datesArray.push( currentDate );
     }
   }
 
   return isSort
     ? datesArray.sort(( a, b ) => new Date(a) > new Date(b) ? 1 : -1 )
     : datesArray;
-}
+};
 
-const futureDates = generateDates(100, 'future', true);
-const pastDates = generateDates(100, 'past', true);
-const unsortedDates = generateDates(100, 'future', false);
-const randomNumber = Math.floor(Math.random() * futureDates.length);
-const randomDate = new Date( futureDates[randomNumber] );
+const Wrapper = ({ children }) => <div className='story-container' style={{ maxWidth: '375px', padding: '20px' }}>{ children }</div>;
 
-storiesOf('Календарь', module)
+storiesOf( 'Календарь', module )
   .addParameters({
     options: {
       theme: {} // this is just a workaround for addon-readme
@@ -49,18 +45,18 @@ storiesOf('Календарь', module)
       sidebar: CalendarReadme,
     },
   })
-  .add('С предстоящими датами', () => (
-    <div className='story-container' style={{ maxWidth: '375px', padding: '20px' }}>
-      <Calendar dates={ futureDates }/>
-    </div>
+  .add( 'С предстоящими датами', () => (
+    <Wrapper>
+      <Calendar dates={ generateDates( 100, 'future', true ) }/>
+    </Wrapper>
   ))
-  .add('С прошедшими датами', () => (
-    <div className='story-container' style={{ maxWidth: '375px', padding: '20px' }}>
-      <Calendar dates={pastDates}/>
-    </div>
+  .add( 'С прошедшими датами', () => (
+    <Wrapper>
+      <Calendar dates={ generateDates( 100, 'past', true ) }/>
+    </Wrapper>
   ))
-  .add('С неотсортированными датами', () => (
-    <div className='story-container' style={{ maxWidth: '375px', padding: '20px' }}>
-      <Calendar dates={unsortedDates}/>
-    </div>
+  .add( 'С неотсортированными датами', () => (
+    <Wrapper>
+      <Calendar dates={ generateDates( 100, 'future', false ) }/>
+    </Wrapper>
   ));
